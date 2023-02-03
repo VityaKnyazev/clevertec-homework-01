4ee768e94439cb5c0929c3edbc16fc3ab2fa98f6
Vitya Knyazev <Utopio@tut.by>
2023-02-03 22:56:35 +0300

    Created feature/producer branch.

--- src/main/java/ru/clevertec/homework01/Git.java
+++ src/main/java/ru/clevertec/homework01/Git.java
@@ -2,6 +2,6 @@ package ru.clevertec.homework01;
 
 public class Git {
     public static void main(String[] args) {
-		System.out.println("Here we starting developing process!");
+		System.out.println("Here we starting work on new feature! Producer will produce goods for delivering");
 	}
 }
