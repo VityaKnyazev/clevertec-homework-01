283b9c5b29a928ed11d8d81f9e7c6e6b7f1adfe3
Vitya Knyazev <Utopio@tut.by>
2023-02-03 22:53:06 +0300

    Added Logistic process in App on car, boat, air.

--- src/main/java/ru/clevertec/homework01/Git.java
+++ src/main/java/ru/clevertec/homework01/Git.java
@@ -1,7 +1,19 @@
 package ru.clevertec.homework01;
 
+import ru.clevertec.homework01.logistic.AirLogistic;
+import ru.clevertec.homework01.logistic.BoatLogistic;
+import ru.clevertec.homework01.logistic.CarLogistic;
+import ru.clevertec.homework01.logistic.Logistic;
+
 public class Git {
     public static void main(String[] args) {
 		System.out.println("Here we starting developing process!");
+		Logistic carLogistic = new CarLogistic();
+		Logistic boatLogistic = new BoatLogistic();
+		Logistic airLogistic = new AirLogistic();
+		
+		carLogistic.organizeDelivery();
+		boatLogistic.organizeDelivery();
+		airLogistic.organizeDelivery();		
 	}
 }
