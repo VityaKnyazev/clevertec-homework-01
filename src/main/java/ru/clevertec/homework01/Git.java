f5e5a97820d905a708d0edf712c357ed86221672
Vitya Knyazev <Utopio@tut.by>
2023-02-03 22:38:12 +0300

    Development branch creaated. Application development in process...

--- src/main/java/ru/clevertec/homework01/Git.java
+++ src/main/java/ru/clevertec/homework01/Git.java
@@ -2,6 +2,6 @@ package ru.clevertec.homework01;
 
 public class Git {
     public static void main(String[] args) {
-		System.out.println("This is our App start version!");
+		System.out.println("Here we starting developing process!");
 	}
 }
