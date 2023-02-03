c443651fe7fd78d6e514152e4d6dd245642304cf
Vitya Knyazev <Utopio@tut.by>
2023-02-03 23:01:10 +0300

    Created feature/consumer branch for future App releases.

--- src/main/java/ru/clevertec/homework01/Git.java
+++ src/main/java/ru/clevertec/homework01/Git.java
@@ -2,6 +2,6 @@ package ru.clevertec.homework01;
 
 public class Git {
     public static void main(String[] args) {
-		System.out.println("Here we starting developing process!");
+    	System.out.println("Here we starting work on new future feature! It's Consumer interfaace.");
 	}
 }
