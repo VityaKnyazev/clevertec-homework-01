c57a893a41a34b128934d1cca5dd2be142f90846
Vitya Knyazev <Utopio@tut.by>
2023-10-06 16:38:38 +0300

    Added calculation interface for Calculator developing.

---
+++ src/main/java/ru/clevertec/knyazev/Calculation.java
@@ -0,0 +1,13 @@
+package ru.clevertec.knyazev;
+
+public interface Calculation {
+
+    Integer getSum(Integer a, Integer b);
+
+    Integer getSubtract(Integer a, Integer b);
+
+    Long getMultiply(Integer a, Integer b);
+
+    Double getDivide(Integer a, Integer b);
+
+}
