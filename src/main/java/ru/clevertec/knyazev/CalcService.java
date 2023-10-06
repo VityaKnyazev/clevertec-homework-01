7c3fc459585d7d6cb983354e877a79caf5277536
Vitya Knyazev <Utopio@tut.by>
2023-10-06 16:16:01 +0300

    Starting Calculation service feature

---
+++ src/main/java/ru/clevertec/knyazev/CalcService.java
@@ -0,0 +1,15 @@
+package ru.clevertec.knyazev;
+
+public interface CalcService {
+
+    /**
+     *
+     * Calculate and return result value
+     *
+     * @param a int value
+     * @param b int value
+     * @return calculation result
+     */
+    Integer calculateAndReturn(int a, int b);
+
+}
