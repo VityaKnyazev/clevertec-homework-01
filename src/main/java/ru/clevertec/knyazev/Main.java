8a538500e176b2522a616ffd6f17c2c7cac07f6c
Vitya Knyazev <Utopio@tut.by>
2023-10-06 13:46:58 +0300

    Starting developing process

--- src/main/java/ru/clevertec/knyazev/Main.java
+++ src/main/java/ru/clevertec/knyazev/Main.java
@@ -1,9 +1,10 @@
 package ru.clevertec.knyazev;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
 
+        System.out.println("Hello world!");
+        System.out.println("Starting developing process!");
+
+    }
 }
